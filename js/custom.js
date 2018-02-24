3baa95ae7268075138ae0da34cc25df75e8676a8
Aditya Nathwani <aditya.nathwani.1@gmail.com>
2018-02-25 00:31:51 +0530

    Update to latest w3fa-template and add menu.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
+if(window.innerWidth<601)
+{
+	//javscript for mobile devices
+}
+if(window.innerWidth>600 && window.innerWidth<993)
+{
+	//javscript for tablet devices
+}
+if(window.innerWidth>992)
+{
+	/*javscript for PC and other large devices*/
+}
 $(document).ready(function(){
 	$("#dc").height($(window).height());
 	$("#dc").width($(window).width());
